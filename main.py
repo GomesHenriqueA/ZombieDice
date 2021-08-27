8e8e8ee7ec4fe3e065eb7d89b3c89caeff7349c3
Henrique <henriqueg.assis@gmail.com>
2021-08-27 10:52:44 -0300

    First Version (Unfinished)

--- main.py
+++ main.py
@@ -1,20 +1,105 @@
-from Dados import *
+import random
+
+# Dados
+dadoVerde = "CPCTPC"
+dadoAmarelo = "TPCTPC"
+dadoVermelho = "TPTCPT"
+
+playGame = False
+
+# Adiciona os 13 dados no saco
+
+saco = []
+
+
+def adicionarDados():
+    for i in range(0, 6):
+        saco.append(dadoVerde)
+    for i in range(0, 4):
+        saco.append(dadoAmarelo)
+    for i in range(0, 3):
+        saco.append(dadoVermelho)
+
+
+if playGame == True:
+    adicionarDados()
 
 # Seleção de Número de Jogadores
-numJog = int(input("Insira o Número de Jogadores: "))
-listaJog = list()
+listPlayers = []
 
+numJog = int(input("Insira o Número de Jogadores: "))
 if numJog < 2:
     print("Número de Jogadores insuficientes!")
 else:
-    for i in range(numJog):
-        listaJog.append(input(f"Jogador N°{i + 1}:"))
+    for ind in range(1, numJog + 1):
+        nome = input(f"Insira o Nome do jogador N° {ind}: ")
+        cerebros = 0
+        vidas = 3
 
+        # Adiciona cada jogador à lista e agrega o placar individual
+        player = [f"Player N°{ind}, Nome: {nome}, Pontos: {cerebros}, Vidas: {vidas}"]
+        listPlayers.append(player)
 
-def ListaJog():
-    print()
-    print("=======Jogadores=======")
-    print(listaJog)
+        playGame = True
 
+    # Começar o Jogo!
+    while playGame == True:
+        adicionarDados()
+        rodada = False
+        i = 1
 
-RolarDados()
+        try:
+            jogadorVez = int(input("Insira o Número do jogador que irá jogar os dados:"))
+            playerAtual = jogadorVez - 1
+
+        except:
+            print("Insira um Valor Válido!")
+
+        if jogadorVez != 0:
+            playGame = True
+            rodada = True
+
+        while rodada == True:
+            try:
+                jogar = int(input("[1] - Rolar os Dados\n[2] - Sair\n"))
+            except:
+                print("Digite um valor Válido!")
+            if jogar == 1:
+                dados = []
+                for i in range(0, 3):
+                    numSorteado = random.randint(1, 12)
+
+                    dadoSorteado = saco[numSorteado]
+
+                    faceDado = random.randint(0, 5)
+
+                    # Verificação das faces sorteadas
+                    try:
+                        if dadoSorteado[faceDado] == "C":
+                            if listPlayers[playerAtual][cerebros] == 13: # Em desenvolvimento
+                                print(f"======= Vitória do Jogador N°{playerAtual} =======")
+                                rodada = False
+                                playGame = False
+                            else:
+                                print(f"Dado {i + 1}: {dadoSorteado[faceDado]}")
+                                print("Voce comeu um Cérebro!")
+                                cerebros = cerebros + 1
+
+                        elif dadoSorteado[faceDado] == "T":
+                            if listPlayers[playerAtual][vidas] == 0: # Em desenvolvimento
+                                print("Você ficou sem vidas! Fim da sua Rodada!")
+                                rodada = False
+                            else:
+                                print(f"Dado {i + 1}: {dadoSorteado[faceDado]}")
+                                print("Você levou um tiro!")
+                                vidas = vidas - 1
+                        else:
+                            print(f"Dado {i + 1}: {dadoSorteado[faceDado]}")
+                            print("Sua vítma fugiu, corra atrás dela!!!")
+                    except:
+                        print("Erro")
+            else:
+                print("Fim de Jogo")
+                rodada = False
+                playGame = False
+                break
